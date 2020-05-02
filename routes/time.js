82ace50ced2402e9654463f2264d1de0595a4b72
Ajil Abdalqader <ajilqado@gmail.com>
2020-05-02 21:04:41 +0200

    creating-button-index.ejs

--- routes/time.js
+++ routes/time.js
@@ -17,7 +17,37 @@ Router.get("/:number/:from/:to",(req,res,next)=>{
     
 })
 
+Router.get("/",(req,res,next)=>{
 
+    let {number,from,to} = req.query
+    try{ 
+        let result= convert(number).from(from).to(to)
+        res.status(200).json({sucess:true,result:result})
+        }
+    catch(err){
+        // res.json({success:false, message:err.message})
+        let customerror={status:416,message:err.message}
+        next(customerror)
+       
+    }
+   
+    
+})
+
+Router.post("/",(req,res,next)=>{
+    let {number,from,to}=req.body
+    try{ 
+        let result= convert(number).from(from).to(to)
+        res.status(200).json({sucess:true,result:result})
+        }
+    catch(err){
+        // res.json({success:false, message:err.message})
+        let customerror={status:416,message:err.message}
+        next(customerror)
+       
+    }
+
+})
 
 
 
